291e78cf2a9a6e869af585c1e4bc33673d749d7c
Dhruv Chadha <dhruvchadha121@gmail.com>
2024-04-29 21:09:26 +0530

    Updated Home and Transactions Page

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,37 @@
+import { getServerSession } from "next-auth";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { HomeCard } from "../../../components/HomeCard";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
 
+async function getBalance() {
+    const session = await getServerSession(authOptions);
+    const balance = await prisma.balance.findFirst({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
 
-export default function() {
-    return <div>
-        Dashboard
+    return {
+        amount: balance?.amount || 0,
+        locked: balance?.locked || 0
+    }
+}
+
+export default async function() {
+    const balance = await getBalance();
+
+    return <div className="w-screen">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+            Your Wallet
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+            <div>
+                <HomeCard />
+            </div>
+            <div>
+                <BalanceCard amount={balance.amount} locked={balance.locked}/>
+            </div>
+        </div>
     </div>
 }
---
+++ apps/user-app/components/HomeCard.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { Card } from "@repo/ui/card"
+import { P2PTransferIcon, TransactionsIcon, TransferIcon } from "../app/(dashboard)/layout"
+import { useRouter } from "next/navigation"
+
+
+export const HomeCard = () => {
+    const router = useRouter();
+
+    return <Card title="Features">
+        <div className="pt-2">
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800" onClick={() => {
+                router.push("/transfer")
+            }}>
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <TransferIcon />
+                Transfer Money
+            </span>
+            </button>
+
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800" onClick={() => {
+                router.push("/transactions")
+            }}>
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <TransactionsIcon />
+                Transactions
+            </span>
+            </button>
+
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800" onClick={() => {
+                router.push("/p2ptransfer")
+            }}>
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <P2PTransferIcon />
+                P2P Transfer
+            </span>
+            </button>
+        </div>
+    </Card>
+}
